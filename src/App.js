bef161e456c7cf211bb04b71d0d1104750343c17
Alice Parrish <aloeasinthevera@gmail.com>
2023-03-31 15:43:55 -0400

    Add bootstrap styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import TopBar from './Components/TopBar';
+import Background from './Components/Background';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import StoreListing from './Components/StoreListing';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar />
+      <Background />
+      <StoreListing />
     </div>
   );
 }
